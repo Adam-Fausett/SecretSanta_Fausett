9d486a8a6fe54a39e23ecf516f081d2e11ab4683
waterfausett <waterfausett@gmail.com>
2020-07-11 01:14:00 -0500

    fixed path typo

---
+++ SecretSanta.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Controllers;
+using System;
+using Xunit;
+
+namespace SecretSanta.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = (ViewResult)controller.Index();
+
+            var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
+
+            var expectedVersion = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            var expectedRuntime = isMono ? "Mono" : ".NET";
+
+            // Assert
+            Assert.Equal(expectedVersion, result.ViewData["Version"]);
+            Assert.Equal(expectedRuntime, result.ViewData["Runtime"]);
+        }
+    }
+}
