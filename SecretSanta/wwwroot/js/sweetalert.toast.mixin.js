3be24eabf77ddf4710b1563b59cf83f06297954f
waterfausett <waterfausett@gmail.com>
2020-07-10 22:24:33 -0500

    bundled more js

---
+++ SecretSanta/wwwroot/js/sweetalert.toast.mixin.js
@@ -0,0 +1,15 @@
+﻿$(() => {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'bottom-end',
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        onOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    });
+
+    window.Toast = Toast;
+});
