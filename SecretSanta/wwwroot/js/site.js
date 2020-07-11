f2fb1a9ae594ba464f099c5f3823876a45f4ec9f
waterfausett <waterfausett@gmail.com>
2020-07-10 23:04:46 -0500

    better scollintoview implementation - promise like (will make it an actual promise soon)

--- SecretSanta/wwwroot/js/site.js
+++ SecretSanta/wwwroot/js/site.js
@@ -4,3 +4,31 @@
 function delayAsync(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+/* Extends Element Objects with a function named scrollIntoViewPromise
+*  options: the normal scrollIntoView options without any changes
+*/
+
+Element.prototype.scrollIntoViewAsync = function (scrollOptions, observerOptions = {}) {
+    this.scrollIntoView(scrollOptions);
+
+    const defaultObserverOptions = {
+        threshold: 1.0
+    };
+
+    let parent = this;
+
+    return {
+        then: function (x) {
+            const intersectionObserver = new IntersectionObserver((entries) => {
+                let [entry] = entries;
+
+                if (entry.isIntersecting) {
+                    setTimeout(() => { x(); intersectionObserver.unobserve(parent) }, 10);
+                }
+            }, Object.assign(defaultObserverOptions, observerOptions));
+
+            intersectionObserver.observe(parent);
+        }
+    };
+}
